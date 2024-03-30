dc0e5fd761821166ee7428c75bbdb7601d560e26
Alzabet, Agustin (Tax) <Agustin.Alzabet@thomsonreuters.com>
2024-03-30 17:54:17 -0300

    account service

--- BE/src/controllers/accounts.controller.js
+++ BE/src/controllers/accounts.controller.js
@@ -1,17 +1,11 @@
 import Account from "../models/account.model.js";
 import jwt from "jsonwebtoken";
-import { getAccountService, createAccountService } from "../services/accounts.service.js";
+import { getAccountsService, getAccountsByOwnerService, getAccountService, createAccountService, deleteAccountService, updateAccountService } from "../services/accounts.service.js";
 
 
 export const getAccounts = async (req, res) => {
     try {
-        // Only Admin get all accounts
-        const { token } = req.cookies;
-        const user = jwt.decode(token);
-        if(!user.role.adminPermission){
-            return res.status(401).json({ message: "Not Authorized." });
-        }
-        const accounts = await Account.find().populate("accountType").populate("owners");
+        const accounts = await getAccountsService(req.user);
         res.json(accounts);
     } catch (error) {
         return res.status(500).json({ message: error.message });
@@ -20,9 +14,7 @@ export const getAccounts = async (req, res) => {
 
 export const getAccountsByOwner = async (req, res) => {
     try {
-        //const accounts = await Account.find({"owners": req.params.ownerId}).populate("accountType").populate("owners");
-        
-        const accounts = await Account.find({"owners": req.params.ownerId}).populate("accountType").populate("owners");
+        const accounts = await getAccountsByOwnerService(req.params.ownerId);
         res.json(accounts);
     } catch (error) {
         return res.status(500).json({ message: error.message });
@@ -44,16 +36,10 @@ export const getAccount = async (req, res) => {
 
 export const createAccount = async (req, res) => {
     try {
-        const { token } = req.cookies;
-        const decodedToken = jwt.decode(token);
-        // Only Admin can create, update and delete accounts
-        if(!decodedToken.role.adminPermission){
-            return res.status(401).json({ message: "Not Authorized." });
-        }
         const { number, owners, accountType, balance } = req.body;
-        const newAccount = new Account({number, owners, accountType, balance });
+        const newAccount = new Account({ number, owners, accountType, balance });
 
-        res.json( await createAccountService(newAccount));
+        res.json(await createAccountService(newAccount, req.user));
 
         //await newAccount.save();
         //res.json(newAccount);
@@ -64,40 +50,19 @@ export const createAccount = async (req, res) => {
 
 export const deleteAccount = async (req, res) => {
     try {
-        const { token } = req.cookies;
-        const decodedToken = jwt.decode(token);
-        // Only Admin can create, update and delete accounts
-        if(!decodedToken.role.adminPermission){
-            return res.status(401).json({ message: "Not Authorized." });
-        }
-        const deletedAccount = await Account.findByIdAndDelete(req.params.id);
-        if (!deletedAccount) {
-            return res.status(404).json({ message: "Account not found" });
-        }
+        await deleteAccountService(req.params.id, req.user);
         return res.sendStatus(204);
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+        return res.status(error.statusCode ? error.statusCode : 500).json({ message: error.message });
     }
 };
 
 export const updateAccount = async (req, res) => {
     try {
-        const { token } = req.cookies;
-        const decodedToken = jwt.decode(token);
-        // Only Admin can create, update and delete accounts
-        if(!decodedToken.role.adminPermission){
-            return res.status(401).json({ message: "Not Authorized." });
-        }
-        const { number, owners, accountType, balance} = req.body;
-        const accountUpdated = await Account.findOneAndUpdate(
-                { _id: req.params.id },
-                { number, owners, accountType, balance},
-                { new: true }
-            );
-        
+        const accountUpdated = await updateAccountService(req.params.id, req.body, req.user);
         return res.json(accountUpdated);
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+        return res.status(error.statusCode ? error.statusCode : 500).json({ message: error.message });
     }
 };
 
--- BE/src/services/accounts.service.js
+++ BE/src/services/accounts.service.js
@@ -1,11 +1,28 @@
 import Account from "../models/account.model.js";
 import { getUsersService } from "./users.service.js";
 
+export const getAccountsService = async (user) => {
+    // Only Admin get all accounts
+    if (!user.role.adminPermission) {
+        throw new Error('Not Authorized.');
+    }
+    return await Account.find().populate("accountType").populate("owners");
+}
+
+export const getAccountsByOwnerService = async (ownerId) => {
+    return await Account.find({ "owners": ownerId }).populate("accountType").populate("owners");
+}
+
 export const getAccountService = async (accountId) => {
     return await Account.findById({ "_id": accountId }).populate("accountType").populate("owners");
 }
 
-export const createAccountService = async (account) => {
+export const createAccountService = async (account, user) => {
+    // Only Admin can create, update and delete accounts
+    if(!user.role.adminPermission){
+        throw {statusCode: 401, message: 'Not Authorized.'};
+    }
+    
     const adminUsers = await getUsersService('admninUsersOnly');
     const adminOnwer = adminUsers.filter((admin) => account.owners.includes(admin._id)).length > 0;
 
@@ -13,6 +30,32 @@ export const createAccountService = async (account) => {
         throw new Error('Admin cannot be set as owner of an acount');
     }
 
-    return await account.save();;
+    return await account.save();
 }
 
+export const deleteAccountService = async (accountId, user) => {
+    // Only Admin can create, update and delete accounts
+    if(!user.role.adminPermission){
+        throw {statusCode: 401, message: 'Not Authorized.'};
+    }
+    console.log("service",user);
+    const deletedAccount = await Account.findByIdAndDelete(accountId);
+    if (!deletedAccount) {
+        throw {statusCode: 404, message: 'Account not found.'};
+    }
+}
+
+export const updateAccountService = async (accountId, account, user) => {
+    // Only Admin can create, update and delete accounts
+    if(!user.role.adminPermission){
+        throw {statusCode: 401, message: 'Not Authorized.'};
+    }
+
+    const accountUpdated = await Account.findOneAndUpdate(
+        { _id: accountId },
+        { number: account.number, owners: account.owners, accountType: account.accountType, balance: account.balance },
+        { new: true }
+    );
+
+    return accountUpdated;
+}
